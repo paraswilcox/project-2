737e2c484a45629be81066bf6f8cbc268398b1bf
paras kant sharma <paraskant.sharma@gmail.com>
2017-07-08 16:23:23 +0530

    Adding menu

--- instabot.py
+++ instabot.py
@@ -3,3 +3,7 @@ import requests
 # adding global variables which will remain constant throughout the program
 APP_ACCESS_TOKEN = "387324467.c39e406.357156de69064cfb861647d1e171d7d7"
 BASE_URL = 'https://api.instagram.com/v1/'
+# creating a variable menu
+menu = "\nChoose from the following options:\n1.View your own details\n2.Get user_id of an instagram user\n" \
+       "3.Retrieve Your latest post\n4.Retrieve a user's latest post\n5.Recent media liked by you\n" \
+       "6.Like a user's post\n7.Get List of comments on a user's post\n8.Comment on a user's post\n9.exit\n"
