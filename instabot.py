85b64e6697ffae4dc36873eb78e2cc27576d5878
paras kant sharma <paraskant.sharma@gmail.com>
2017-07-08 16:20:50 +0530

    Adding global variables

--- instabot.py
+++ instabot.py
@@ -1,2 +1,5 @@
 # importing requests library to make http requests
 import requests
+# adding global variables which will remain constant throughout the program
+APP_ACCESS_TOKEN = "387324467.c39e406.357156de69064cfb861647d1e171d7d7"
+BASE_URL = 'https://api.instagram.com/v1/'
