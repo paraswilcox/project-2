f7a945119375aa51ffa1e0ee81184f007228ae34
paras kant sharma <paraskant.sharma@gmail.com>
2017-07-08 17:09:17 +0530

    Adding life to get_list_of_comments_on_users_post(insta_username) function

--- instabot.py
+++ instabot.py
@@ -103,9 +103,20 @@ def like_a_users_post(insta_username):
         print("Like not successful")
 
 
-# hollow function to get a list of comments on a user's recent post
+# full fledged function to get a list of comments on a user's recent post
 def get_list_of_comments_on_users_post(insta_username):
-    pass
+    post_id = get_user_recent_posts(insta_username)
+    print("post_id = ", post_id)
+    request_url = BASE_URL + "media/{}/comments?access_token={}".format(post_id, APP_ACCESS_TOKEN)
+    print("Requesting:\n{}".format(request_url))
+    comments_on_this_post = requests.get(request_url).json()
+    if comments_on_this_post["meta"]["code"] == 200:
+        for i in range(len(comments_on_this_post)):
+            print(comments_on_this_post["data"][i]["from"]["username"], end=" : ")
+            print(comments_on_this_post["data"][i]["text"])
+            print()
+    else:
+        print("Status code other than 200")
 
 
 # hollow function to comment on a user's recent post
