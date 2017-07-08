3d633ba63bcd0c70dc9ba8f2b77bd092adf2718e
paras kant sharma <paraskant.sharma@gmail.com>
2017-07-08 16:27:59 +0530

    designing a function which will show menu

--- instabot.py
+++ instabot.py
@@ -1,5 +1,6 @@
 # importing requests library to make http requests
 import requests
+
 # adding global variables which will remain constant throughout the program
 APP_ACCESS_TOKEN = "387324467.c39e406.357156de69064cfb861647d1e171d7d7"
 BASE_URL = 'https://api.instagram.com/v1/'
@@ -7,3 +8,32 @@ BASE_URL = 'https://api.instagram.com/v1/'
 menu = "\nChoose from the following options:\n1.View your own details\n2.Get user_id of an instagram user\n" \
        "3.Retrieve Your latest post\n4.Retrieve a user's latest post\n5.Recent media liked by you\n" \
        "6.Like a user's post\n7.Get List of comments on a user's post\n8.Comment on a user's post\n9.exit\n"
+
+
+# creating High level design of function show_menu()
+def show_menu():
+    insta_username = input("Enter the username for which you want to perform these actions")
+    if len(insta_username) > 0:
+        choice = int(input(menu))
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            pass
+        elif choice == 6:
+            pass
+        elif choice == 7:
+            pass
+        elif choice == 8:
+            pass
+        elif choice == 9:
+            exit(code="Application Closed")
+        else:
+            exit(code="You did'nt entered one of the choices above")
+    else:
+        exit(code="You have to enter a username")
